8810c4dcabcd90066a96693763a4a6834bb67934
coughlinalbert1 <111651444+coughlinalbert1@users.noreply.github.com>
2024-04-23 13:23:11 -0400

    Add files via upload

--- transaction/main.py
+++ transaction/main.py
@@ -1,9 +1,11 @@
+import asyncio, datetime, logging, os, requests
+from datetime import timedelta, datetime
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import jwt, JWTError
 from pydantic import BaseModel
 from redis_om import get_redis_connection, HashModel
-import os, requests, httpx, logging, asyncio
 from typing import Optional
 from dotenv import load_dotenv
 
@@ -20,10 +22,13 @@ PASSWORD = os.getenv('REDIS_PASSWORD')
 HOST = os.getenv('REDIS_HOST')
 
 app = FastAPI(
-    title="Account Service", 
+    title="Transaction Service", 
     version="1.0", 
-    description="Account Service API using FastAPI and Redis"
+    description="Transaction Service using FastAPI and Redis"
 )
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 origins = [
     ACCOUNT,
     TRANSACTION,
@@ -45,14 +50,9 @@ redis = get_redis_connection(
     decode_responses=True
 )
 
-class Account(BaseModel):
-    username: str
-    email: str
-    password: str
-    phone_num: Optional[str] = None
-    first_name: str
-    last_name: str
-
+'''
+Data models for requests, response, and redis schema
+'''
 class AccountModel(HashModel):
     user_id: str
     username: str
@@ -63,38 +63,25 @@ class AccountModel(HashModel):
     last_name: str
     balance: Optional[float] = 0.0
     access: str = ""
-    time_expires: Optional[str] = None
 
     class Meta: 
         database = redis
 
-class AccountResponse(BaseModel):
-    user_id: int
-    username: str
-    email: str
-    hashed_password: str
+class TransferRequest(BaseModel):
+    receiver_username: str
+    amount: float
 
-def format_response(account: AccountModel):
-    return {
-        'user_id': account.user_id,
-        'username': account.username,
-        'email': account.email,
-        'phone_num': account.phone_num,
-        'first_name': account.first_name,
-        'last_name': account.last_name,
-        'balance': account.balance
-    }
+class DepositRequest(BaseModel):
+    amount: float
 
-def get_user(user_id: str):
-    all_keys = redis.scan_iter("*")
-    keys = [key.split(":")[-1] for key in all_keys if "AccountModel" in key]
-    if not keys:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No user found")
-    for key in keys:
-        user = AccountModel.get(key.split(":")[-1])
-        if user.user_id == user_id:
-            return user
-    return None
+class WithdrawRequest(BaseModel):
+    amount: float
+
+
+#####################################################################
+'''
+Utility Functions
+'''
 
 def get_user_by_name(username: str):
     all_keys = redis.scan_iter("*")
@@ -107,66 +94,23 @@ def get_user_by_name(username: str):
             return user
     return None
 
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
-@app.post("/create/")
-async def account(request: Account):
-    url = f'{AUTHENTICATION}/register/'
-    headers = {'Content-Type': 'application/json'}
-    data = {
-        'username': request.username,
-        'password': request.password
-    }
-    req = requests.post(url, json=data, headers=headers)
-
-    if req.status_code == status.HTTP_400_BAD_REQUEST:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="username is taken")
-    elif req.status_code == status.HTTP_404_NOT_FOUND:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User registration failed")
-    
-    response = req.json()
-    user_id = response.get('user_ID')
-    hashed_password = response.get('password')
-
-    if not user_id:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User ID is missing in the response")
-    if not hashed_password:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Hashed password is missing in the response")
-    
-    account = AccountModel(
-        user_id=user_id,
-        username=request.username,
-        hashed_password=hashed_password,
-        email=request.email,
-        phone_num=request.phone_num,
-        first_name=request.first_name,
-        last_name=request.last_name
-    )
-    try:
-        await asyncio.to_thread(account.save)
-        logging.info(f'Account saved for {account.username}')
-    except Exception as e:
-        logging.error(f'Failed to save account for {account.username}: {str(e)}')
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to create account")
-    
-    return format_response(account)
-
-@app.get("/account/{user_id}")
-async def get_account(user_id: str):
-    account = get_user(user_id)
-    if not account:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Account not found")
-    return format_response(account)
-
-
-@app.post("/login/")
-async def login(account: OAuth2PasswordRequestForm = Depends()):
+def call_authentication_service(username: str, password: str):
     url = f'{AUTHENTICATION}/login/'
-    headers = {'Content-Type': 'application/json'}
     data = {
-        'username': account.username,
-        'password': account.password
+        'username': username,
+        'password': password
     }
-    req = requests.post(url, json=data, headers=headers)
+    req = requests.post(url, data=data)
 
     if req.status_code == status.HTTP_404_NOT_FOUND:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
@@ -174,31 +118,130 @@ async def login(account: OAuth2PasswordRequestForm = Depends()):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
     
     response = req.json()
-    
-    account = get_user_by_name(response['username'])
-    if not account:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Account not found")
+    print(response)
+    return response
 
-    account.access = response['access_token']
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     try:
-        await asyncio.to_thread(account.save)
-        logging.info(f'Access token saved for {account.username}')
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = get_user_by_name(username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+#####################################################################
+
+'''
+API Endpoints
+'''
+
+@app.post("/token/", response_model=None)
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    if not call_authentication_service(form_data.username, form_data.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    user = get_user_by_name(form_data.username)
+    access_token_expires = timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.post("/deposit", response_model=None)
+async def deposit_funds(deposit_request: DepositRequest, current_user: AccountModel = Depends(get_current_user)):
+    user = get_user_by_name(current_user.username)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    if user.username != current_user.username:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Unauthorized user")
+    user.balance += deposit_request.amount
+
+    try:
+        await asyncio.to_thread(user.save)
+        logging.info(f'Account saved for {user.username}')
     except Exception as e:
-        logging.error(f'Failed to save access token for {account.username}: {str(e)}')
+        logging.error(f'Failed to save account for {user.username}: {str(e)}')
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to create account")
+    return {
+        "message": "Deposit successful",
+        "username": user.username,
+        "account": user.user_id,
+        "balance": user.balance
+    }
+
+@app.post("/withdraw", response_model=None)
+async def withdraw_funds(withdraw_request: WithdrawRequest, current_user: dict = Depends(get_current_user)):
+    user = get_user_by_name(current_user.username)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    if user.username != current_user.username:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Unauthorized user")
+    user.balance -= withdraw_request.amount
+
+    try:
+        await asyncio.to_thread(user.save)
+        logging.info(f'Account update for {user.username}')
+    except Exception as e:
+        logging.error(f'Failed to update account for {user.username}: {str(e)}')
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to update account")
+    return {
+        "message": f"Withdrawl successful: {withdraw_request.amount}",
+        "username": user.username,
+        "account": user.user_id,
+        "balance": user.balance
+    }
+
+@app.post("/transfer", response_model=None)
+async def transfer_funds(transfer_request: TransferRequest, current_user: dict = Depends(get_current_user)):
+    sender = get_user_by_name(current_user.username)
+    if not sender:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    if sender.username != current_user.username:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Unauthorized user")
+    receiver = get_user_by_name(transfer_request.receiver_username)
+    if not sender or not receiver:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    if sender.balance < transfer_request.amount:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Insufficient funds")
+    sender.balance -= transfer_request.amount
+    receiver.balance += transfer_request.amount
+
+    try:
+        await asyncio.to_thread(sender.save)
+        logging.info(f'Account update for {sender.username}')
+    except Exception as e:
+        logging.error(f'Failed to update account for {sender.username}: {str(e)}')
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to update account")
     
-    account_info = format_response(account)
-    
+    try:
+        await asyncio.to_thread(receiver.save)
+        logging.info(f'Account update for {sender.username}')
+    except Exception as e:
+        logging.error(f'Failed to update account for {receiver.username}: {str(e)}')
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to update account")
     return {
-        'access_token': response['access_token'],
-        'token_type': response['token_type'],
-        'username': account_info['username'],
-        'user_id': account_info['user_id'],
-        'email': account_info['email'],
-        'phone_num': account_info['phone_num'],
-        'first_name': account_info['first_name'],
-        'last_name': account_info['last_name'],
-        'balance': account_info['balance']
+        "message": f"Withdrawl successful: {transfer_request.amount}",
+        "username": sender.username,
+        "account": sender.user_id,
+        "balance": sender.balance
     }
+
+
+
+
+
 
 
